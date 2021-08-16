51558e92315f63474b3d8f222c2854ffb378fce0
professor339 <65883904+professor339@users.noreply.github.com>
2021-08-16 11:01:43 +0530

    Create news.js

---
+++ news.js
@@ -0,0 +1,33 @@
+<script>
+(function () {
+   var CharacterPos = 0;
+   var MsgBuffer = "";
+   var TypeDelay = 150; 
+   var NxtMsgDelay = 100;
+   var MsgIndex = 0;
+   var delay;
+   var MsgArray = ["News Headlines\n\n1. RDIF and Wockhardt partner to produce Sputnik V and Sputnik Light vaccines\n\n2. Dishman Carbogen Amcis records 16.1 per cent growth YoY in consolidated net sales\n\n3.DoP takes measures to mandatorily revise ceiling prices of drugs every five years\n\n"];
+
+   function StartTyping() {
+      var id = document.getElementById("typing-text");
+      if (CharacterPos != MsgArray[MsgIndex].length) {
+         MsgBuffer  = MsgBuffer + MsgArray[MsgIndex].charAt(CharacterPos);
+         id.value = MsgBuffer+"_";
+         delay = TypeDelay;
+         id.scrollTop = id.scrollHeight; 
+      } else {
+         delay = NxtMsgDelay;
+         MsgBuffer   = "";
+         CharacterPos = -1;
+         if (MsgIndex!=MsgArray.length-1){
+           MsgIndex++;
+         }else {
+           MsgIndex = 0;
+         }
+       }
+       CharacterPos++;
+       setTimeout(StartTyping,delay);
+   }
+StartTyping();
+})();
+</script>
