84f77f7ef4f0ab281a4e76b916c5b7a9fd7c8128
professor339 <65883904+professor339@users.noreply.github.com>
2021-08-31 11:10:25 +0530

    Update news.js

--- news.js
+++ news.js
@@ -5,7 +5,7 @@
    var NxtMsgDelay = 100;
    var MsgIndex = 0;
    var delay;
-   var MsgArray = ["28th August Update\n\n1.GPAT 2017 part 5 Question paper is available now\n\n2. One word nanoparticles question is now available in M.pharm section\n\n3. Goa Drug Inspector 2020 question is available for practice."] function StartTyping() {
+   var MsgArray = ["28th August Update\n\n1.GPAT 2017 part 5 Question paper is available now\n\n2.One word nanoparticles question is now available in M.pharm section\n\n3. Goa Drug Inspector 2020 question is available for practice"] function StartTyping() {
       var id = document.getElementById("typing-text");
       if (CharacterPos != MsgArray[MsgIndex].length) {
          MsgBuffer  = MsgBuffer + MsgArray[MsgIndex].charAt(CharacterPos);
